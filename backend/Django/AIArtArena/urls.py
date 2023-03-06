737a8902b58d3c3c66c19e44c6f5321090394ae0
cmstiver <chayse.stiver@gmail.com>
2023-03-06 03:04:51 -0800

    now serves images

--- backend/Django/AIArtArena/urls.py
+++ backend/Django/AIArtArena/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 from AIArtArena import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = format_suffix_patterns([
     path('posts/', views.PostList.as_view(), name='post-list'),
 ])
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
